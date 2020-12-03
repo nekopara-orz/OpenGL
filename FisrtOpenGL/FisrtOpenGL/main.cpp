f9ec71bbd9f078558237a0794e2d34ec838e44ea
“Zhang” <1070168724@qq.com>
2020-12-03 18:18:50 +0800

    完成Texture创建

--- FisrtOpenGL/FisrtOpenGL/main.cpp
+++ FisrtOpenGL/FisrtOpenGL/main.cpp
@@ -4,20 +4,21 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include "Shader.h"
 
 float vertices[] = {
--0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
- 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
- 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 0.1f,
- //0.5f, -0.5f, 0.0f,
- //0.0f,  0.5f, 0.0f,
- 0.8f,  0.8f, 0.0f, 1.0f, 0.0f, 1.0f
+	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
 };
 
 unsigned int indices[] = {
-	0,1,2,2,1,3
+	0,1,2,2,3,0
 };
 
 
@@ -70,14 +71,58 @@ int main()
 
 
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)) );
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)) );
 	glEnableVertexAttribArray(1);
 
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
+	unsigned int TexBufferA;
+	glGenTextures(1,&TexBufferA);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, TexBufferA);
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		
+	}
+	else
+	{
+		printf("load img failed");
+	}
+	stbi_image_free(data);
+
+	unsigned  int  TexBufferB;
+	glGenTextures(1, &TexBufferB);
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_2D, TexBufferB);
+	unsigned char* data2 = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+
+	if (data2)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+	}
+	else
+	{
+		printf("load img failed");
+	}
+	stbi_image_free(data2);
+	
 	//glUseProgram(shaderProgram);
+
+
+
+
 	myShader.use();
 
 	while (!glfwWindowShouldClose(window))
@@ -87,10 +132,15 @@ int main()
 		//rendering
 		glClearColor(0.4f, 0.6f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, TexBufferA);
+		glActiveTexture(GL_TEXTURE3);
+		glBindTexture(GL_TEXTURE_2D, TexBufferB);
 
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-			
+		
 		//float timeValve = glfwGetTime();
 		//float greenValve = (sin(timeValve) / 2.0) + 0.5;
 		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
@@ -100,6 +150,8 @@ int main()
 		myShader.use();
 
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glUniform1i(glGetUniformLocation(myShader.ID, "ourTexture"), 0);
+		glUniform1i(glGetUniformLocation(myShader.ID, "ourFace"), 3);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
---
+++ FisrtOpenGL/FisrtOpenGL/stbTest.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+//int maintest()
+//{
+//	int width, height, nrChannels;
+//	stbi_set_flip_vertically_on_load(true);
+//	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+//	for (size_t i = 0; i < 50; i++)
+//	{
+//		printf("%d\n", (int)data[i]);
+//	}
+//	stbi_image_free(data);
+//	return 0;
+//}
